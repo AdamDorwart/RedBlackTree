e700b828d4e9c5b4eb803617e5af255801e1a32e
Adam Dorwart <adorwart@ucsd.edu>
2016-03-30 02:26:48 -0700

    First implementation of RBTree insert. Incorrectly balances. Tester has tree print aid.

--- rbtree.h
+++ rbtree.h
@@ -5,7 +5,7 @@
 template<typename T, typename KeyT>
 struct RBNode {
     RBNode() {
-        isRed = false;
+        isRed = true;
         key = KeyT();
         data = T();
         left = nullptr;
@@ -87,14 +87,101 @@ class RBTree {
             }
         }
         
-        RBNode<T, KeyT>* insert(const T& data, const KeyT& key) {
-            // Insert node as root
+        RBNode<T, KeyT>* insert(const KeyT& key, const T& data) {
             RBNode<T, KeyT>* newRBNode;
+            
             if (!root) {
+                // Insert node as root
                 root = new RBNode<T, KeyT>();
                 root->key = key;
                 root->data = data;
+                root->isRed = false;
                 newRBNode = root;
+            } else {
+                // Traverse Tree (DFS)
+                RBNode<T, KeyT> *curNode, *parentNode, **nextNode;
+                curNode = root;
+                bool locationFound = false;
+                while (!locationFound) {
+                    if (key < curNode->key) {
+                        // Key is less : Go left
+                        parentNode = curNode;
+                        nextNode = &(curNode->left);
+                    } else if (curNode->key < key) {
+                        // Key is more : Go right
+                        parentNode = curNode;
+                        nextNode = &(curNode->right);
+                    } else {
+                        // Key is equal : Replace Data
+                        curNode->data = data;
+                        locationFound = true;
+                    }
+                    
+                    if (!*nextNode) {
+                        // No next node : Insert new node here
+                        *nextNode = new RBNode<T, KeyT>();
+                        curNode = *(nextNode);
+                        curNode->key = key;
+                        curNode->data = data;
+                        curNode->parent = parentNode;
+                        locationFound = true;
+                    }
+                    
+                    curNode = *(nextNode);
+                }
+                
+                // Rebalance Tree
+                while (curNode != root) {
+                    RBNode<T, KeyT>* parent = curNode->parent;
+                    if (parent->isRed) {
+                        RBNode<T, KeyT>* gparent = getGrandparent(curNode);
+                        RBNode<T, KeyT>* uncle = getUncle(curNode, gparent);
+                        
+                        if (uncle && uncle->isRed) {
+                            // Parent is red and Uncle is Red
+                            // Change both to black and GP to red if not root and continue from GP
+                            parent->isRed = false;
+                            uncle->isRed = false;
+                            if (gparent != root) {
+                                gparent->isRed = true;
+                            }
+                            curNode = gparent;
+                            // Continue from GP
+                        } else {
+                            // Parent is red and Uncle is Black
+                            if (curNode == parent->right && parent == gparent->left) {
+                                // curNode is right child of parent, parent is left child of gparent
+                                
+                                leftRotate(parent);
+
+                                curNode = curNode->left;
+                                parent = curNode->parent;
+                                gparent = getGrandparent(curNode);
+                                                    
+                            } else if (curNode == parent->left && parent == gparent->right) {
+                                // curNode is left child of parent, parent is right child gparent
+                                
+                                rightRotate(parent);
+
+                                curNode = curNode->right;
+                                parent = curNode->parent;
+                                gparent = getGrandparent(curNode);
+                            }
+                            // curNode is left child of parent, parent is left child of gparent
+                            parent->isRed = false;
+                            gparent->isRed = true;
+                            if (curNode == parent->left) {
+                                rightRotate(gparent);
+                            } else {
+                                leftRotate(gparent);
+                            }
+                            curNode = root;
+                        }
+                    } else {
+                        curNode = root;
+                    }
+                }
+                
             }
             return newRBNode;
         }
@@ -107,14 +194,69 @@ class RBTree {
             
         }
         
+        RBNode<T, KeyT>* getRoot() {
+            return root;
+        }
+        
     private:
         RBNode<T, KeyT>* root;
         
-        void leftRotate(RBNode<T, KeyT>* node) {
+        inline void leftRotate(RBNode<T, KeyT>* node) {
+            if (!node->right) {
+                return;
+            }
+            RBNode<T, KeyT>* child = node->right;
+            RBNode<T, KeyT>* parent = node->parent;
+            if (!parent) {
+                root = child;
+            } else if (parent->left == node) {
+                parent->left = child;
+            } else {
+                parent->right = child;
+            }
             
+            node->right = child->left;
+            if (node->right) {
+                node->right->parent = node;
+            }
+            
+            child->left = node;
+            child->left->parent = child;
         }
         
-        void rightRotate(RBNode<T, KeyT>* node) {
+        inline void rightRotate(RBNode<T, KeyT>* node) {
+            if (!node->left) {
+                return;
+            }
+            RBNode<T, KeyT>* child = node->left;
+            RBNode<T, KeyT>* parent = node->parent;
+            if (!parent) {
+                root = child;
+            } else if (parent->left == node) {
+                parent->left = child;
+            } else {
+                parent->right = child;
+            }
             
+            node->left = child->right;
+            if (node->left) {
+                node->left->parent = node;  
+            }
+            
+            child->right = node;
+            child->right->parent = child;
+        }
+        
+        inline RBNode<T, KeyT>* getGrandparent(RBNode<T, KeyT>* node) {
+            if (node && node->parent) {
+                return node->parent->parent;
+            } else {
+                return nullptr;
+            }
+        }
+        
+        inline RBNode<T, KeyT>* getUncle(RBNode<T, KeyT>* node, RBNode<T, KeyT>* gp) {
+            if (!gp) return nullptr;
+            return  (gp->left == node->parent) ? gp->right : gp->left;
         }
 };
--- rbtree_test.cpp
+++ rbtree_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include "rbtree.h"
 
 using namespace std;
@@ -6,12 +7,41 @@ using namespace std;
 using Tree = RBTree<int>;
 using Node = RBNode<int, int>;
 
+// Print a BST to stdout
+// Inspired by http://stackoverflow.com/questions/13484943/print-a-binary-tree-in-a-pretty-way 
+void PostOrderPrint(Node* node, int indent=0) {
+    const int indentSize = 5;
+    if(node) {
+        if(node->left) PostOrderPrint(node->left, indent+indentSize);
+        if (indent) {
+            cout << setw(indent) << ' ';
+        }
+        if (node->isRed) {
+            cout << "{R," << node->key << "}\n ";
+        } else {
+            cout << "{B," << node->key << "}\n ";
+        }
+        if(node->right) PostOrderPrint(node->right, indent+indentSize);
+    }
+}
+
+
 int main(int argc, char** argv) {
     RBTree<int> tree;
     
-    Node* root = tree.insert(1, 1);
+    tree.insert(1, 1);
+    tree.insert(2, 1);
+    tree.insert(3, 1);
+    tree.insert(4, 1);
+    tree.insert(5, 1);
+    tree.insert(6, 1);
+    tree.insert(7, 1);
+    tree.insert(8, 1);
+    tree.insert(9, 1);
+    tree.insert(10, 1);
     
-    cout << "Root value: " << root->data << endl;
+    
+    PostOrderPrint(tree.getRoot());
     
     return 0;
 }
